7ae25e4ef2e7d25480b1baf41d8b64a68f841513
Saeko <saeconomail@gmail.com>
2021-01-30 15:48:42 +0900

    DBをindex.phpに表示できるように設定

--- server/public/index.php
+++ server/public/index.php
@@ -7,4 +7,36 @@ require_once('functions.php');
 // DBに接続
 $dbh = connectDb(); // 特にエラー表示がなければOK
 
+$sql = 'select * from tasks';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+$tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>タスク管理</title>
+</head>
+<body>
+	<h1>タスク管理アプリ</h1>
+
+	<h2>未完了タスク</h2>
+	<ul>
+		<?php foreach($tasks as $task): ?>
+		<?php if($task['status'] == 'notyet'): ?>
+			<li>
+				<?php echo h($task['title']); ?>
+			</li>
+		<?php endif; ?>
+		<?php endforeach; ?>
+	</ul>
+	<hr>
+
+	<h2>完了したタスク</h2>
+
+</body>
+</html>
