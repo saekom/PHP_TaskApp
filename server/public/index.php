f15c5222eb7e3b84d37f0691c4c1c1eaa9f2a7e6
Saeko <saeconomail@gmail.com>
2021-01-31 11:40:53 +0900

    メモを新規追加できるようにした

--- server/public/index.php
+++ server/public/index.php
@@ -12,6 +12,32 @@ $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+// 新規タスク追加
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	// フォームに入力されたデータの受け取り
+	$title = $_POST['title'];
+
+	// エラーチェック用の配列
+	$errors = array();
+
+	// バリデーション
+	if ($title == '') {
+		$errors['title'] = 'タスク名を入力してください';
+	}
+
+	if (empty($errors)) {
+		$dbh = connectDb();
+		$sql = "insert into tasks (title, created_at,   updated_at) values (:title, now(), now())";
+		$stmt = $dbh->prepare($sql);
+		$stmt->bindParam(':title', $title);
+		$stmt->execute();
+
+		// index.phpに戻る
+		header('Location: index.php');
+		exit;
+	}
+}
+
 ?>
 
 
@@ -24,10 +50,18 @@ $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <body>
 	<h1>タスク管理アプリ</h1>
 
+	<p>
+		<form action="" method="POST">
+			<input type="text" name="title">
+			<input type="submit" value="追加">
+			<span style="color: red;"><?php echo h($errors['title']); ?></span>
+		</form>
+	</p>
+
 	<h2>未完了タスク</h2>
 	<ul>
-		<?php foreach($tasks as $task): ?>
-		<?php if($task['status'] == 'notyet'): ?>
+		<?php foreach ($tasks as $task): ?>
+		<?php if ($task['status'] == 'notyet'): ?>
 			<li>
 				<?php echo h($task['title']); ?>
 			</li>
