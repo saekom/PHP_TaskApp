034cb52bd65616290ff2acaaec9c3df147fde68b
Saeko <saeconomail@gmail.com>
2021-01-31 12:21:40 +0900

    タスク編集にバリデーション機能を追加

--- server/public/edit.php
+++ server/public/edit.php
@@ -17,6 +17,34 @@ $stmt->execute();
 // 結果の取得
 $post = $stmt->fetch(PDO::FETCH_ASSOC);
 
+// タスクの編集
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// 受け取ったデータ
+	$title = $_POST['title'];
+
+	// エラーチェック用の配列
+	$errors = array();
+
+	// バリデーション
+	if ($title == $post['title']) {
+		$errors['title'] = 'タスク名が変更されていません。';
+	}
+
+	// エラーが1つもなければレコードを更新
+	if (empty($errors)) {
+		// DBに接続
+		$dbh = connectDb();
+		$sql = "update tasks set title = :title, updated_at = now() where id = :id";
+		$stmt = $dbh->prepare($sql);
+		$stmt->bindParam(':title', $title);
+		$stmt->bindParam(':id', $id);
+		$stmt->execute();
+
+		header('Location: index.php');
+	    exit;
+	}
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +59,7 @@ $post = $stmt->fetch(PDO::FETCH_ASSOC);
 		<form action="" method="POST">
 			<input type="text" name="title" value="<?php echo h($post['title']); ?>">
 			<input type="submit" value="編集">
+			<span style="color:red;"><?php echo h($errors['title']); ?></span>
 		</form>
 	</p>
 </body>
